e13aa8a7b0d3be04003e422e0c729318b8993617
Julie Thrall <juliethrall10@gmail.com>
2019-09-04 22:19:41 -0700

    completed posts/middleware

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -17,8 +17,8 @@ router.get('/', (req, res) => {
 router.post('/:id', validatePost, (req, res) => {
     const { id } = req.params
     const post = req.body;
+
     post.user_id = id
-    console.log(post)
 
     db.insert(post)
         .then(newPost => {
@@ -30,11 +30,52 @@ router.post('/:id', validatePost, (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {});
+router.get('/:id', validatePostId, (req, res) => {
+    const { id } = req.params
 
-router.delete('/:id', (req, res) => {});
+    db.getById(id)
+        .then(post => {
+            res.status(200).json(post)
+        })
+        .catch(e => {
+            res.status(500).json({error: 'error getting post from database'})
+        })
+});
 
-router.put('/:id', (req, res) => {});
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    let postRemoved = {}
+
+    db.getById(id)
+        .then(post => {
+            postRemoved = post
+        })
+        .catch(e => {
+            res.status(500).json({error: 'error getting post by id'})
+        })
+
+    db.remove(id)
+        .then(result => {
+            res.status(200).json({deleted: [result, postRemoved]})
+        })
+        .catch(e => {
+            res.status(500).json({error: 'error deleting post'})
+        })
+});
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const changes = req.body
+    console.log(changes)
+
+    db.update(id, changes)
+        .then(updatedPost => {
+            res.status(200).json(updatedPost)
+        })
+        .catch(e => {
+            res.status(500).json({error: 'error updating post in database'})
+        })
+});
 
 // custom middleware
 function isEmpty(obj) {
@@ -55,6 +96,24 @@ function validatePost (req, res, next) {
     next()
 }
 
-function validatePostId (req, res, next) {}
+function validatePostId (req, res, next) {
+    let id = req.params.id
+    let post = {}
+    db.getById(id)
+        .then(result => {
+            if (result) {
+                post = req.post;
+                console.log('post validated')
+            } else {
+                res.status(404).json({ message: "invalid post id" })
+            }
+        })
+        .catch(e => {
+            res.status(500).json({error: 'error accessing specified post in database'})
+        })
+    
+
+    next()
+}
 
 module.exports = router;
